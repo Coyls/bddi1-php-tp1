034a290d293098995e34d2fb20149c23882b1f0c
Coyls <76538870+Coyls@users.noreply.github.com>
2021-02-03 09:21:06 +0100

    rename: random

---
+++ guessNumber.php
@@ -0,0 +1,22 @@
+<?php
+
+$counter = 0;
+$numberToGuess = rand(0,100);
+
+do {
+    $counter++;
+    $line = trim(fgets(STDIN));
+
+    if ($numberToGuess > $line) {
+        echo "too low \n";
+    }else if ($numberToGuess < $line){
+        echo "too hight \n";
+    } else {
+        echo "You win";
+        break;
+        die();
+    }
+
+} while(true);
+
+echo "in ".$counter." attempts!\n";
